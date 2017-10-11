371383b3a7f70da5ed162dc30635ddd0fea553c9
Michael Hinrichs <michaelghinrichs@gmail.com>
2017-10-11 08:20:38 -0700

    Styling done

--- src/App.js
+++ src/App.js
@@ -10,28 +10,45 @@ class App extends Component {
     this.state = {
       numberOfDays: 0,
       result: "",
+      resultDays: 0,
     }
+  }
+
+  getExcludedDates = () => {
+    return excludedDates.map(item => item.date);
   }
 
   calculateDate = () => {
     const calculatedDate = moment()
-      .addWorkdays(this.state.numberOfDays, excludedDates)
+      .addWorkdays(this.state.numberOfDays, this.getExcludedDates())
       .format('MM-DD-YYYY');
 
-    this.setState({ result: calculatedDate })
+    this.setState({
+      result: calculatedDate,
+      resultDays: this.state.numberOfDays,
+    })
   }
 
   setNumberOfDays = ({target}) => {
     this.setState({ numberOfDays: target.value });
   }
+
   render() {
     return (
-      <div className="App">
-        <div className="Flex">
-          <div>
+      <div className="App Flex">
+        <div className="Sidebar Flex Stack">
+          <h3>
             Excluded Dates:
-          </div>
-          <div>
+          </h3>
+          <div className="SidebarContent">
+            <div className="Flex Split-Around">
+              <div>
+                Date:
+              </div>
+              <div>
+                Reason:
+              </div>
+            </div>
             {excludedDates.map(item => (
               <div className="Flex Card Split">
                 <div>{item.date}</div>
@@ -40,18 +57,29 @@ class App extends Component {
             ))}
           </div>
         </div>
-        <div>
-          <input
-            type="number"
-            onChange={this.setNumberOfDays}
-            value={this.state.numberOfDays}
-          />
-          <button onClick={this.calculateDate}>
-            Calculate Date
-          </button>
-          { this.state.result &&
-              <div>In {this.state.numberOfDays} working school days it will be {this.state.result}</div>
-          }
+        <div className="Main bg">
+          <div class="Content Stack" >
+            <div className="Control">
+              <input
+                className="Input round-s"
+                type="number"
+                onChange={this.setNumberOfDays}
+                value={this.state.numberOfDays}
+              />
+              <button
+                onClick={this.calculateDate}
+                className="button button--ujarak button--border-medium button--round-s button--text-thick">Calculate Date</button>
+            </div>
+            <div className="Result Flex">
+              { this.state.result &&
+                  <div className="ResultContent Flex Stack">
+                    <div>In <b>{this.state.resultDays}</b> working school days it will be:</div>
+                    <div className="Date">{moment(this.state.result).format('dddd, MMMM Do YYYY')}</div>
+                    <div className="Date">{this.state.result}</div>
+                  </div>
+              }
+            </div>
+          </div>
         </div>
       </div>
     );
