b53d140f002d9c9b73cb9b37a7ee6dfdcb2afc57
Hannes Widrig <hanneswidrig@gmail.com>
2017-10-14 21:20:30 -0400

    Fixed minor errors and Add a date picker for input #7

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import moment from 'moment';
+/*eslint-disable no-unused-vars*/
 import weekdayCalc from 'moment-weekday-calc';
+/*eslint-enable no-unused-vars*/
 import excludedDates from './excluded_dates.json';
+import DatePicker from 'react-datepicker';
+
 import './App.css';
+import 'react-datepicker/dist/react-datepicker.css';
 
 class App extends Component {
   constructor(props){
@@ -11,22 +16,68 @@ class App extends Component {
       numberOfDays: 0,
       result: "",
       resultDays: 0,
+      startDate: moment(),
+      todayDate: moment()
     }
   }
 
+  handleChange = (date) => {
+    this.setState ({
+      startDate: date,
+    })
+  }
+  
+  isWeekday = (date) => {
+    const day = date.day()
+    return day !== 0 && day !== 6
+  }
+
   getExcludedDates = () => {
-    return excludedDates.map(item => item.date);
+    return this.state.data.map(item => item.date);
+  }
+
+  getParams = () => {
+    const searchParams = window.location.search.replace('?', '');
+    return searchParams;
+  };
+
+  decodeToState = () => {
+    const params = this.getParams();
+    const decodedData = params === ""
+      ? []
+      : JSON.parse(window.atob(params));
+
+    return decodedData;
+  }
+
+  encodeState = (data) => {
+    const encoded = window.btoa(JSON.stringify(data));
+    return encoded;
+  }
+
+  addToUrl = () => {
+    const myNewUrlQuery = this.encodeState(this.state.data);
+
+    if (window.history.pushState) {
+      const newurl = `${window.location.protocol}//${window.location.host}${window.location.pathname}?${myNewUrlQuery}`;
+      window.history.pushState({ path:newurl },'',newurl);
+    }
   }
 
   calculateDate = () => {
-    const calculatedDate = moment()
-      .addWorkdays(this.state.numberOfDays, this.getExcludedDates())
-      .format('MM-DD-YYYY');
+    const resultDays = moment().isoWeekdayCalc({
+      rangeStart: moment().format('DD MMM YYYY'),
+      rangeEnd: this.state.startDate.format('DD MMM YYYY'),
+      weekdays: [1,2,3,4,5],
+      exclusions: this.getExcludedDates()
+    });
+
+    const calculatedDate = moment(this.state.startDate).format('MM-DD-YYYY');
 
     this.setState({
       result: calculatedDate,
       resultDays: this.state.numberOfDays,
-    })
+    });
   }
 
   setNumberOfDays = ({target}) => {
@@ -37,9 +88,17 @@ class App extends Component {
     return (
       <div className="App Flex">
         <div className="Sidebar Flex Stack">
-          <h3>
-            Excluded Dates:
-          </h3>
+          <div className="Flex Split">
+            <h3>
+              Excluded Dates:
+            </h3>
+            <button
+              onClick={this.addToUrl}
+              className="button button--ujarak button--border-medium button--round-s button--text-thick"
+            >
+              Save
+            </button>
+          </div>
           <div className="SidebarContent">
             <div className="Flex Split-Around">
               <div>
@@ -49,31 +108,39 @@ class App extends Component {
                 Reason:
               </div>
             </div>
-            {excludedDates.map(item => (
-              <div className="Flex Card Split">
-                <div>{item.date}</div>
-                <div>{item.reason}</div>
-              </div>
-            ))}
+            {this.state.data.length === 0
+                ? <div>No dates to exclude</div>
+                : this.state.data.map(item => (
+                  <div key={item.date} className="Flex Card Split">
+                    <div>{item.date}</div>
+                    <div>{item.reason}</div>
+                  </div>
+                ))
+            }
           </div>
         </div>
         <div className="Main bg">
-          <div class="Content Stack" >
+          <div className="Content Stack">
             <div className="Control">
-              <input
-                className="Input round-s"
-                type="number"
-                onChange={this.setNumberOfDays}
-                value={this.state.numberOfDays}
+              <DatePicker
+              selected={this.state.startDate}
+              onChange={this.handleChange}
+              filterDate={this.isWeekday}
+              excludeDates={this.getExcludedDates()}
+              minDate={this.state.todayDate}
+              readOnly
               />
               <button
                 onClick={this.calculateDate}
-                className="button button--ujarak button--border-medium button--round-s button--text-thick">Calculate Date</button>
+                className="button button--ujarak button--border-medium button--round-s button--text-thick"
+              >
+                Calculate Date
+              </button>
             </div>
             <div className="Result Flex">
               { this.state.result &&
                   <div className="ResultContent Flex Stack">
-                    <div>In <b>{this.state.resultDays}</b> working school days it will be:</div>
+                    <div>In <b>{this.state.resultDays}</b> working days it will be:</div>
                     <div className="Date">{moment(this.state.result).format('dddd, MMMM Do YYYY')}</div>
                     <div className="Date">{this.state.result}</div>
                   </div>
